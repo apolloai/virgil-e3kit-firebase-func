9a91a4294297680a4c54d37992356050fc993d08
Thomas Klaner <thomas@apollo.ai>
2020-08-11 18:21:58 +0200

    move function to europe

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,7 +4,7 @@ import { generateVirgilJwt } from './generate-virgil-jwt';
 
 admin.initializeApp();
 
-export const getVirgilJwt = functions.https.onCall(async (_data, context) => {
+export const getVirgilJwt = functions.region('europe-west1').https.onCall(async (_data, context) => {
   if (!context.auth) {
     // Throwing an HttpsError so that the client gets the error details.
     throw new functions.https.HttpsError('unauthenticated', 'The function must be called ' +
